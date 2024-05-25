584149df419f7ee97745ac599c5d5c89785f7e76
Rhythm1821 <alamrawat1821@gmail.com>
2024-05-25 16:03:52 +0530

    Project setup

---
+++ components/Hero.tsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+    return (
+        <div className="bg-black-100 flex justify-center item-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+            <div className="pb-20 pt-36 ">
+                <h1>Everl</h1>
+            </div>
+        </div>
+    );
+}
